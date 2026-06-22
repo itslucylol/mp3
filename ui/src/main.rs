dc3437e784a76fdbdbecaf24f9a102094880f87e
Lucy <87684856+itslucylol@users.noreply.github.com>
2026-06-22 10:12:06 -0400

    create main layout

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -3,15 +3,15 @@ slint::include_modules!();
 fn main() -> Result<(), slint::PlatformError> {
     let ui = MainWindow::new()?;
 
-    // Handle the button click
-    ui.on_button_clicked({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            // Update the Slint property from Rust
-            ui.set_my_text("Hello from Rust!".into());
-        }
-    });
+    // // Handle the button click
+    // ui.on_button_clicked({
+    //     let ui_handle = ui.as_weak();
+    //     move || {
+    //         let ui = ui_handle.unwrap();
+    //         // Update the Slint property from Rust
+    //         ui.set_my_text("Hello from Rust!".into());
+    //     }
+    // });
 
     ui.run()
 }
