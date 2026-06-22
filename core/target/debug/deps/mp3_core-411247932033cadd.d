01000c6e4c8c0b1f34bf97d28645f129545a72a8
Lucy <87684856+itslucylol@users.noreply.github.com>
2026-06-22 09:32:19 -0400

    remove core/target (why was that included)

--- core/target/debug/deps/mp3_core-411247932033cadd.d
+++
@@ -1,5 +0,0 @@
-/home/lucy/Documents/GitHub/mp3/core/target/debug/deps/mp3_core-411247932033cadd.d: src/main.rs
-
-/home/lucy/Documents/GitHub/mp3/core/target/debug/deps/libmp3_core-411247932033cadd.rmeta: src/main.rs
-
-src/main.rs:
